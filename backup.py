177d8b2e3322485ad371e00a306dcf39b28e5424
excelkks <excelkks@hotmail.com>
2020-08-27 13:44:09 +0800

    自动备份于 2020年08月27日 13:44:09

--- backup.py
+++ backup.py
@@ -18,10 +18,15 @@ nvim_file = os.path.join(home_dir, '.config/nvim/init.vim')
 nvim_backup = os.path.join(repo, 'vim/nvim/init.vim')
 coc_settings = os.path.join(home_dir, '.config/nvim/coc-settings.json')
 coc_settings_backup = os.path.join(repo, 'vim/nvim/coc-settings.json')
+vimspector_file = os.path.join(home_dir, '.confg/nvim/vimspector_sample')
+vimspector_backup = os.path.join(repo, 'vim/nvim/vimspector_sample')
 nvim_backup_cmd = 'cp ' + nvim_file + ' ' + nvim_backup
 coc_backup_cmd = 'cp ' + coc_settings + ' ' + coc_settings_backup
+vimspector_backup_cmd = 'cp -rf ' + vimspector_file + ' ' + vimspector_backup
 os.system(nvim_backup_cmd)
 os.system(coc_backup_cmd)
+os.system(vimspector_backup_cmd)
+
 
 
 message = time.strftime('自动备份于 %Y年%m月%d日 %H:%M:%S', time.localtime(time.time()))
